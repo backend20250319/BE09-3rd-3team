675346309afda7b78ce68c730e8c72321fd70798
junbem <junbem0728@naver.com>
2025-06-13 17:41:54 +0900

    userId 댓글과 같이 출력

--- comment-service/src/main/java/com/ohgiraffers/commentservice/controller/CommentController.java
+++ comment-service/src/main/java/com/ohgiraffers/commentservice/controller/CommentController.java
@@ -1,9 +1,13 @@
 package com.ohgiraffers.commentservice.controller;
 
 import com.ohgiraffers.commentservice.dto.CommentRequestDto;
+import com.ohgiraffers.commentservice.dto.CommentResponseDto;
+import com.ohgiraffers.commentservice.entity.Comment;
 import com.ohgiraffers.commentservice.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,28 +19,34 @@ public class CommentController {
 
     // 1. 댓글 등록
     @PostMapping
-    public ResponseEntity<String> create(@RequestBody CommentRequestDto dto) {
+    public ResponseEntity<CommentResponseDto> create(@RequestBody CommentRequestDto dto,
+                                                     @AuthenticationPrincipal String userId) {
         try {
-            dto.setCreatedUserId("test");
-            commentService.create(dto);
-            String message = "댓글 등록됨\n댓글: " + dto.getContent();
-            return ResponseEntity.ok(message);
+            dto.setCreatedUserId(userId);
+            Comment comment = commentService.create(dto);
+            CommentResponseDto response = CommentResponseDto.fromEntity(comment);
+            return ResponseEntity.ok(response);
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.badRequest().build();
         }
     }
+
 
     // 2. 댓글 수정
     @PutMapping("/{id}")
     public ResponseEntity<String> update(@PathVariable Long id,
                                          @RequestBody CommentRequestDto dto) {
         try {
-            commentService.update(id, dto);
-            return ResponseEntity.ok("댓글 수정됨");
+            Comment updatedComment = commentService.update(id, dto);
+            String message = String.format(
+                    "댓글 수정됨\n댓글 내용: %s", updatedComment.getContent()
+            );
+            return ResponseEntity.ok(message);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
+
 
     // 3. 댓글 삭제
     @DeleteMapping("/{id}")
