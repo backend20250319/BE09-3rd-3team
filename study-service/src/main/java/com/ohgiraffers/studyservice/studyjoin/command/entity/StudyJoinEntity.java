d87dbdb37ef3ca8709155eb501255bedf582b24f
jungho <kiik52@naver.com>
2025-06-13 21:41:02 +0900

    feat : 신청한 스터디 내역 조회 진행 중

--- study-service/src/main/java/com/ohgiraffers/studyservice/studyjoin/command/entity/StudyJoinEntity.java
+++ study-service/src/main/java/com/ohgiraffers/studyservice/studyjoin/command/entity/StudyJoinEntity.java
@@ -22,7 +22,7 @@ public class StudyJoinEntity {
     @Column(name = "user_id")
     private String userId;
 
-    @Column(name = "study_room_id", unique = true)
+    @Column(name = "study_room_id")
     private Long studyRoomId;
 
     @Enumerated(EnumType.STRING)
