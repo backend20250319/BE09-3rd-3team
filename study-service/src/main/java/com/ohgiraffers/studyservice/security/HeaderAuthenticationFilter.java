a82130ad5cc8541a10e429843031feb8c5feb665
choseokgeun <chosuekgeun@gmail.com>
2025-06-13 16:56:26 +0900

    포스트맨 작동 이상 무

--- study-service/src/main/java/com/ohgiraffers/studyservice/security/HeaderAuthenticationFilter.java
+++ study-service/src/main/java/com/ohgiraffers/studyservice/security/HeaderAuthenticationFilter.java
@@ -16,41 +16,22 @@ import java.util.List;
 
 @Component
 @Slf4j
-//public class HeaderAuthenticationFilter extends OncePerRequestFilter {
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-//            throws ServletException, IOException {
-//        // API Gateway가 전달한 헤더 읽기
-//        String userId = request.getHeader("X-User-Id");
-//
-//        log.info("userId : {}", userId);
-//
-//        if (userId != null) {
-//            // 이미 Gateway에서 검증된 정보로 인증 객체 구성
-//            PreAuthenticatedAuthenticationToken authentication =
-//                    new PreAuthenticatedAuthenticationToken(userId, null);
-//            SecurityContextHolder.getContext().setAuthentication(authentication);
-//        }
-//        filterChain.doFilter(request, response);
-//    }
-//}
-
-
 public class HeaderAuthenticationFilter extends OncePerRequestFilter {
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
-
-        // 개발 중이므로 무조건 통과 (혹은 임시 헤더 체크만)
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        // API Gateway가 전달한 헤더 읽기
         String userId = request.getHeader("X-User-Id");
-        if (userId == null) {
-            // 임시 유저 ID 설정
-            userId = "123";
-        }
 
-        // SecurityContext 설정 안 해도 됨 → 인증 미사용으로 처리
+        log.info("userId : {}", userId);
+
+        if (userId != null) {
+            // 이미 Gateway에서 검증된 정보로 인증 객체 구성
+            PreAuthenticatedAuthenticationToken authentication =
+                    new PreAuthenticatedAuthenticationToken(userId, null);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
         filterChain.doFilter(request, response);
     }
 }
